9c1be28d3815ab96334403bcb7cb80f1ca0f1fc3
ElianaSalinas <stephaniamontero84@gmail.com>
2025-03-23 19:58:33 -0400

    Agregar archivos de proyecto.

---
+++ Capa_Datos/Conexion.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Data;
+using System.Data.SqlClient;
+namespace Conexion
+{
+    public class CD_Conexion
+    {
+        public SqlConnection Conexion = new SqlConnection("Server=.; Database = BD_Empleados; Integrated Security =true");
+
+        public SqlConnection AbrirConexion()
+        {
+            if (Conexion.State == ConnectionState.Closed)
+                Conexion.Open();
+            return Conexion;
+        }
+
+        public SqlConnection CerrarConexion()
+        {
+            if (Conexion.State == ConnectionState.Open)
+                Conexion.Close();
+            return Conexion;
+        }
+    }
+}
