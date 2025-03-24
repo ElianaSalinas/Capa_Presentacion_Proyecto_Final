bda4407f8ba9aaaf5f600d2a8a5e0bd3e6f05c51
ElianaSalinas <stephaniamontero84@gmail.com>
2025-03-23 22:43:09 -0400

    Version 1.2

---
+++ Capa_Negocio/Interface_Empleados.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Capa_Negocio
+{
+    public interface Interface_Empleados
+    {
+        DataTable MostrarUsu();
+        void InsertarUsu(string nombre, string telefono, string edad);
+        void EditarUsu(int id, string nombre, string telefono, string edad);
+        void EliminarUsu(int id);
+    }
+}
