044f0d04937355c34d11d2f590b9ede0431fcc67
jerez129 <japf20092004@gmail.com>
2025-03-25 02:40:32 -0300

    Version 2.1 Carpetas añadidas para mayor organizacions, ClasesDAL, Capa entidades

--- Capa_Presentacion_Proyecto_Final/FrmLogin.Designer.cs
+++ Capa_Presentacion_Proyecto_Final/FrmLogin.Designer.cs
@@ -28,12 +28,77 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
-            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Text = "FrmLogin";
+            btnIniciarsesión = new Button();
+            lblUsuario = new Label();
+            lblClave = new Label();
+            txtUsuario = new TextBox();
+            txtClave = new TextBox();
+            SuspendLayout();
+            // 
+            // btnIniciarsesión
+            // 
+            btnIniciarsesión.Location = new Point(327, 286);
+            btnIniciarsesión.Name = "btnIniciarsesión";
+            btnIniciarsesión.Size = new Size(112, 29);
+            btnIniciarsesión.TabIndex = 0;
+            btnIniciarsesión.Text = "Iniciar Sesión";
+            btnIniciarsesión.UseVisualStyleBackColor = true;
+            btnIniciarsesión.Click += btnIniciarsesión_Click;
+            // 
+            // lblUsuario
+            // 
+            lblUsuario.AutoSize = true;
+            lblUsuario.Location = new Point(214, 142);
+            lblUsuario.Name = "lblUsuario";
+            lblUsuario.Size = new Size(62, 20);
+            lblUsuario.TabIndex = 1;
+            lblUsuario.Text = "Usuario:";
+            // 
+            // lblClave
+            // 
+            lblClave.AutoSize = true;
+            lblClave.Location = new Point(228, 196);
+            lblClave.Name = "lblClave";
+            lblClave.Size = new Size(48, 20);
+            lblClave.TabIndex = 2;
+            lblClave.Text = "Clave:";
+            // 
+            // txtUsuario
+            // 
+            txtUsuario.Location = new Point(314, 142);
+            txtUsuario.Name = "txtUsuario";
+            txtUsuario.Size = new Size(144, 27);
+            txtUsuario.TabIndex = 3;
+            // 
+            // txtClave
+            // 
+            txtClave.Location = new Point(314, 193);
+            txtClave.Name = "txtClave";
+            txtClave.Size = new Size(144, 27);
+            txtClave.TabIndex = 4;
+            // 
+            // FrmLogin
+            // 
+            AutoScaleDimensions = new SizeF(8F, 20F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(800, 450);
+            Controls.Add(txtClave);
+            Controls.Add(txtUsuario);
+            Controls.Add(lblClave);
+            Controls.Add(lblUsuario);
+            Controls.Add(btnIniciarsesión);
+            Name = "FrmLogin";
+            Text = "FrmLogin";
+            ResumeLayout(false);
+            PerformLayout();
         }
 
         #endregion
+
+        private Button btnIniciarsesión;
+        private Label lblUsuario;
+        private Label lblClave;
+        private TextBox txtUsuario;
+        private TextBox txtClave;
     }
 }
--- Capa_Presentacion_Proyecto_Final/FrmLogin.cs
+++ Capa_Presentacion_Proyecto_Final/FrmLogin.cs
@@ -4,9 +4,11 @@ using System.ComponentModel;
 using System.Data;
 using System.Drawing;
 using System.Linq;
+using System.Security.Cryptography.X509Certificates;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using Capa_Negocio;
 
 namespace Capa_Presentacion_Proyecto_Final
 {
@@ -15,6 +17,24 @@ namespace Capa_Presentacion_Proyecto_Final
         public FrmLogin()
         {
             InitializeComponent();
+        }
+
+        private void btnIniciarsesión_Click(object sender, EventArgs e)
+        {
+           CN_Login.UsuarioNegocio usuarioNegocio = new CN_Login.UsuarioNegocio();
+
+            if (usuarioNegocio.IniciarSesion(txtUsuario.Text, txtClave.Text))
+            {
+                MessageBox.Show("Bienvenido");
+                FrmPrincipal frmPrincipal = new FrmPrincipal();
+                frmPrincipal.Show();
+                this.Hide();
+            }
+            else
+            {
+                MessageBox.Show("Usuario o contraseña incorrectos");
+            }
+
         }
     }
 }
